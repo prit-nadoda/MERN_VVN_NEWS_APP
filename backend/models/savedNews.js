63e62487f1453afb6b2c7ec958ea0ef221a95128
prit-nadoda <prit.nadoda.123@gmail.com>
2024-05-11 00:20:40 +0530

    User Model Design

---
+++ backend/models/savedNews.js
@@ -0,0 +1,14 @@
+//Demo RESPONSE
+// {
+//     "source": {
+//       "id": null,
+//       "name": "Hindustan Times"
+//     },
+//     "author": "HT Sports Desk",
+//     "title": "IPL 2024, PBKS vs RCB Live Score: Virat Kohli misses out on 100th century | Crickit - Hindustan Times",
+//     "description": "IPL 2024, PBKS vs RCB Live Score, Punjab Kings vs Royal Challengers Bengaluru: Play has resumed after rain briefly interrupted the match in Dharamsala.",
+//     "url": "https://www.hindustantimes.com/cricket/ipl-2024-pbks-vs-rcb-ipl-live-score-match-58-punjab-kings-vs-royal-challengers-bengaluru-scorecard-ipl-match-live-9-may-101715245323217.html",
+//     "urlToImage": "https://www.hindustantimes.com/ht-img/img/2024/05/09/550x309/CRICKET-IND-IPL-T20-BENGALURU-GUJARAT-34_1715262662756_1715262684104.jpg",
+//     "publishedAt": "2024-05-09T16:12:23Z",
+//     "content": "IPL 2024, PBKS vs RCB Live Score, Punjab Kings vs Royal Challengers Bengaluru: The Punjab Kings won the toss and chose to bowl first as they look for a win in Dharamsala. Sam Curran continues to lead… [+35598 chars]"
+//   }
---
+++ backend/models/userModel.js
@@ -0,0 +1,49 @@
+import { Schema, model } from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: [true, "Given username is taken!"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Email is already registered!"],
+    validate: [validator.isEmail, "Provide a valid email!"],
+  },
+  fullname: {
+    type: String,
+    required: true,
+    minlength: [4, "Fullname must consist atleast 4 characters!"],
+    maxlength: [25, "Fullname must consist atleast 4 characters!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must consist atleast 6 characters!"],
+    maxlength: [25, "Password must consist atleast 25 characters!"],
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.generateJsonWebToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+export const User = model("users", userSchema);

af1a7157eb567d1f788acf52311780e5faeb9b8c
prit-nadoda <prit.nadoda.123@gmail.com>
2024-05-17 15:24:36 +0530

    Profile + Register Done with API call

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,14 +1,97 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Context } from "../main";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { MdEmail } from "react-icons/md";
+import axios from "axios";
+import { IoIosArrowForward } from "react-icons/io";
+import { button } from "@material-tailwind/react";
+import Loader from "../components/Loader";
 
 const Profile = () => {
-  const { isAuthenticated, setIsAuthenticated, setActiveLink } =
+  const { isAuthenticated, setIsAuthenticated, setActiveLink, user } =
     useContext(Context);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const toastDisplayedRef = useRef(false);
+  const [savedArticles, setSavedArticles] = useState([]);
 
   useEffect(() => {
     setActiveLink("profile");
-  });
-  return <div>Profile</div>;
+    if (!isAuthenticated) {
+      if (!toastDisplayedRef.current) {
+        toast.info("Please login to view your profile");
+        toastDisplayedRef.current = true; // Set the ref to true after displaying toast
+      }
+      navigate("/login", { replace: true }); // Ensure navigation is replacing the current history entry
+    }
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/savedNews",
+          { withCredentials: true }
+        );
+        setSavedArticles(response.data.articles);
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [isAuthenticated, navigate, setActiveLink]);
+
+  if (!isAuthenticated) {
+    return null; // Avoid rendering the profile section if the user is not authenticated
+  }
+
+  return (
+    <section className="bg-[#faf4e4] pt-[80px] md:pt-[30px] dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ">
+        <div className="w-full shadow-2xl bg-white rounded-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl  font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <div className="w-full flex flex-col gap-1 justify-center items-center p-3">
+                <div className="rounded-full shadow-2xl flex justify-center items-center h-[140px] w-[140px] bg-blue-700 text-[#f9f9f9] font-bold text-6xl">
+                  {user.fullname
+                    .split(" ")
+                    .map((word) => word.charAt(0))
+                    .join("")}
+                </div>
+                <h2 className="text-xl font-bold mt-4 leading-tight tracking-tight text-gray-900 md:text-4xl dark:text-white">
+                  {user.fullname}
+                </h2>
+                <h3 className="text-sm font-semibold leading-tight tracking-tight text-gray-500 md:text-2xl dark:text-white">
+                  {`@${user.username}`}
+                </h3>
+                <h3 className="text-sm font-bold mt-4 flex justify-center items-center gap-1 leading-tight tracking-tight text-gray-900 md:text-lg dark:text-white">
+                  <MdEmail size={24} className="text-blue-700" /> {user.email}
+                </h3>
+                {loading && <Loader />}
+                {savedArticles.length > 0 ? (
+                  <Link
+                    to={"/saved"}
+                    className="w-full mt-5 flex justify-center items-center text-sm gap-2 font-normal text-md bg-blue-700 text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300"
+                  >
+                    View {savedArticles.length} Saved Articles
+                    <IoIosArrowForward size={24} />{" "}
+                  </Link>
+                ) : (
+                  <h3 className="text-sm font-bold mt-4 flex justify-center items-center gap-1 leading-tight tracking-tight text-gray-500 md:text-lg dark:text-white">
+                    {" "}
+                    You have no saved articles{" "}
+                  </h3>
+                )}
+
+                <button></button>
+              </div>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,49 @@
-import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../main";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Register = () => {
   const { isAuthenticated, setIsAuthenticated, setActiveLink } =
     useContext(Context);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    fullname: "",
+    username: "",
+  });
+  const navigateTo = useNavigate();
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const { email, password, fullname, username } = user;
+    try {
+      const response = await axios
+        .post(
+          "http://localhost:4000/api/v1/user/register",
+          { email, password, fullname, username },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((response) => {
+          toast.success(response.data.message);
+          setIsAuthenticated(true);
+          navigateTo("/");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setIsAuthenticated(false);
+    }
+  };
 
   useEffect(() => {
     setActiveLink("");
-  });
+  }, [setActiveLink]);
   return (
     <section className="bg-[#faf4e4] pt-[40px] dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ">
@@ -26,6 +61,8 @@ const Register = () => {
                   Your fullname
                 </label>
                 <input
+                  onChange={(e) => handleChange(e)}
+                  value={user.fullname}
                   type="text"
                   name="fullname"
                   id="fullname"
@@ -42,6 +79,8 @@ const Register = () => {
                   create username
                 </label>
                 <input
+                  onChange={(e) => handleChange(e)}
+                  value={user.username}
                   type="text"
                   name="username"
                   id="username"
@@ -58,6 +97,8 @@ const Register = () => {
                   Your email
                 </label>
                 <input
+                  onChange={(e) => handleChange(e)}
+                  value={user.email}
                   type="email"
                   name="email"
                   id="email"
@@ -74,6 +115,8 @@ const Register = () => {
                   create password
                 </label>
                 <input
+                  onChange={(e) => handleChange(e)}
+                  value={user.password}
                   type="password"
                   name="password"
                   id="password"
@@ -84,10 +127,11 @@ const Register = () => {
               </div>
 
               <button
+                onClick={(e) => handleRegister(e)}
                 type="submit"
                 className="w-full bg-blue-500 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Sign in
+                Register
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
@@ -96,7 +140,7 @@ const Register = () => {
                     href="#"
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
-                    Sign in
+                    Login
                   </a>
                 </Link>
               </p>
